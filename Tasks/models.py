9edc4a68125ed467fa058192251262b75d7d260c
dusembekd <dusembekdd@gmail.com>
2020-02-22 23:38:29 +0600

    added task table

---
+++ Tasks/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=25)
+    content = models.CharField(max_length=2048)
+    answer = models.CharField(max_length=512)
+    image = models.ImageField(blank=True)
+
+
+
+# Create your models here.
