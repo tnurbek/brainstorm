0e6b8b43ee2a58cf89cb86858c8cf4e148d967b1
Nurbek <nurbekspage@gmail.com>
2020-02-23 23:51:50 +0600

    base index password.html

--- Brainstorm/urls.py
+++ Brainstorm/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('group', include())
+    path('group/', include('Group.urls'), name='group'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Group/views.py
+++ Group/views.py
@@ -1,18 +1,45 @@
 from django.shortcuts import render
-from django.contrib.auth.models import Group
+from Group.models import Group
+from Option.models import Option
+from Task.models import Task
 from User.models import Player
-from django.http import HttpResponse
 
 # g1 = Group.objects.create(name='')
 # users = Player.objects.all()
 
-def create_group(request, pk):
-    name = request.GET.get('name_group')
-    g = Group.objects.create(name=name)
-    owner = Player.objects.get(pk=pk)
-    g.user_set.add(owner)
+def initial(request):
+    options = Option.objects.all()
+    return render(request, 'base.html', {'options':options})
 
-    return HttpResponse()
+def create_group(request):
+
+    # if request.GET.get('group_name'):
+    #     name = request.GET.get('group_name')
+    # else:
+    #     name = 'dwfwefewf'
+
+    name = request.GET.get('group_name')
+    # money = request.GET.get('money_field')
+    # start = request.GET.get('start_time')
+    start = '19:25:25'
+    option_name = 'public'
+    # option_name = request.GET.get('select')
+
+    print(option_name)
+    print(name)
+    option = Option.objects.get(name=option_name)
+
+    money = 0
+    # start = '12:23:23'
+    # option = Option.objects.get(pk=2)
+    task_id = Task.objects.get(pk=1)
+    g = Group.objects.create(name=name, money=money, start=start, task_id=task_id, option_id=option)
+    owner = Player.objects.get(username=request.user.username)
+    g.user_set.add(owner)
+    if option.name == 'private':
+        return render(request, 'group/password.html')
+    elif option.name == 'public':
+        return render(request, 'group/index.html')
 
 
 # Create your views here.
